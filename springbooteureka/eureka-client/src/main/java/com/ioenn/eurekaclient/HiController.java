3e4d5dde4b2bec6ef75d7ca9cffdc9db63da0c43
cuichaoge <cuichaoge@dhcc.com.cn>
2020-07-02 10:22:17 +0800

    微服务之 eureka-server 和 eureka-client

---
+++ springbooteureka/eureka-client/src/main/java/com/ioenn/eurekaclient/HiController.java
@@ -0,0 +1,16 @@
+package com.ioenn.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    @Value("${server.port}")
+    String port;
+    @GetMapping("hi")
+    public String home (){
+        return "Hi,idea"+ port;
+    }
+}
