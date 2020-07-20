3982b328240cbea54dc992442c316e43e8a50bdc
cuichaoge <cuichaoge@dhcc.com.cn>
2020-07-20 18:18:55 +0800

    提交 Spring Client 集成 Feign 声明式调用。

---
+++ feignserver/eureka-ribbon-client/src/main/java/com/ioenn/eurekaribbonclient/RibbonController.java
@@ -0,0 +1,28 @@
+package com.ioenn.eurekaribbonclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RibbonController {
+
+    @Autowired
+    RibbonService ribbonService;
+    @GetMapping("hi")
+    public String hi(@RequestParam(required = false,defaultValue = "ioenn") String name){
+        return ribbonService.hi(name);
+    }
+
+    @Autowired
+    private LoadBalancerClient loadBalancer;
+    @GetMapping("/testRibbon")
+    public String testRibbon(){
+        ServiceInstance instance = loadBalancer.choose("eureka-client");
+        return instance.getHost()+":"+instance.getPort();
+    }
+
+}
